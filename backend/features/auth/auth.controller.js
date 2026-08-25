eb923645da63d209cbf2967d36672371d1ffd3bd
Charitha Kalam <charithakalam24@gmail.com>
2026-08-25 11:34:44 +0530

    feat: setup backend and basic authentication

---
+++ backend/features/auth/auth.controller.js
@@ -0,0 +1,22 @@
+const authService = require("./auth.service");
+
+const register = async (req, res) => {
+  try {
+    const user = await authService.registerUser(req.body);
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  register,
+};
